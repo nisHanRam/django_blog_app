783a787146b8dab0d03537829c34007e25831412
Nishant Choubisa <nishant.choubisa@arcgate.com>
2026-01-02 10:50:29 +0530

    linking pages, static urls and completing post detail page

--- blog/views.py
+++ blog/views.py
@@ -1,56 +1,76 @@
 from django.shortcuts import render
+from datetime import date
 
-blogs = [
+all_posts = [
     {
-        "id": 1,
+        "slug": "scale-app",
+        "author": "Nishant",
+        "date": date(2021, 7, 21),
+        "image": "scale_web_apps.png",
+        "category": "Web Development",
         "title": "Building Scalable Web Applications in 2025",
-        "description": (
+        "excerpt": (
             "Modern web applications are expected to handle rapid growth, high traffic, "
             "and evolving feature requirements. Scalability must be designed from day one. "
-            "A strong architecture with clear separation between frontend, backend, and "
-            "database layers enables teams to scale efficiently while maintaining performance "
-            "and code quality."
         ),
-        "category": "Web Development",
-        "publish_date": "2025-01-10",
-        "image": "blog/images/scale_web_apps.png",
+        "content": """
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+                There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.
+            """,
     },
     {
-        "id": 2,
-        "title": "Why UI/UX Design Matters More Than Ever",
-        "description": (
-            "In a competitive digital landscape, users expect clarity, simplicity, and "
-            "accessibility. UI/UX design directly impacts user trust, engagement, and "
-            "retention by focusing on intuitive navigation, consistent layouts, and "
-            "problem-solving design principles."
-        ),
+        "slug": "design",
+        "author": "Nishant",
+        "date": date(2021, 2, 2),
+        "image": "design.jpg",
         "category": "Design & UX",
-        "publish_date": "2025-02-02",
-        "image": "blog/images/design.jpg",
+        "title": "Why UI/UX Design Matters More Than Ever",
+        "excerpt": (
+            "In a competitive digital landscape, users expect clarity, simplicity, and "
+            "accessibility. UI/UX design directly impacts user trust and engagement. "
+        ),
+        "content": """
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+                There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.
+            """,
     },
     {
-        "id": 3,
-        "title": "The Rise of Automation in Digital Products",
-        "description": (
-            "Automation is reshaping how digital products are built and scaled. From "
-            "automated testing to AI-powered workflows, automation improves consistency, "
-            "reduces manual effort, and allows teams to focus on high-impact work such as "
-            "innovation and user experience."
-        ),
+        "slug": "automation",
+        "author": "Nishant",
+        "date": date(2021, 3, 15),
+        "image": "automation.png",
         "category": "Technology",
-        "publish_date": "2025-03-05",
-        "image": "blog/images/automation.png",
+        "title": "The Rise of Automation in Digital Products",
+        "excerpt": (
+            "Automation is reshaping how digital products are built and scaled. From "
+            "automated testing to AI-powered workflows, automation improves consistency. "
+        ),
+        "content": """
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+                There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.
+            """,
     },
 ]
 
 
+def get_date(post):
+    return post["date"]
+
+
 def homepage(request):
-    return render(request, "blog/homepage.html", {"blogs": blogs})
+    sorted_posts = sorted(all_posts, key=get_date)
+    latest_posts = sorted_posts[-3:]
+    return render(request, "blog/homepage.html", {"latest_posts": latest_posts})
 
 
 def posts(request):
-    return render(request, "blog/all-posts.html", {"blogs": blogs})
+    return render(request, "blog/all-posts.html", {"all_posts": all_posts})
 
 
-def post_detail(request):
-    pass
+def post_detail(request, slug):
+    identified_post = next(post if post["slug"] == slug else None for post in all_posts)
+    identified_post = next(post for post in all_posts if post["slug"] == slug)
+    return render(request, "blog/post-detail.html", {"post": identified_post})
